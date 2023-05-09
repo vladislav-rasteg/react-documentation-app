08dc527a482a5b94ca46e48605d9e444db9fa550
VladislavRas <rastegaev.design@mail.ru>
2023-05-10 01:33:23 +0500

    fix: shadow for search results list block

--- src/components/CodeSnippet/CodeSnippet.tsx
+++ src/components/CodeSnippet/CodeSnippet.tsx
@@ -1,22 +1,22 @@
 import s from './CodeSnippet.module.css';
 import { ReactComponent as CopyIcon } from './assets/icon_copy.svg';
-import { isButtonElement } from 'react-router-dom/dist/dom';
-import { ALL } from 'dns';
+import { ReactComponent as CopiedIcon } from './assets/icon_copied.svg';
 
 interface CodeSnippetProps {
     code: any;
   }
 
-function copyCode(event: React.MouseEvent<HTMLButtonElement>){
-    var code = String(event.currentTarget.parentElement?.lastElementChild?.lastElementChild?.innerHTML).replaceAll("&amp;", '&',).replaceAll("&gt;", '>').replaceAll("&lt;", '<').replaceAll("&quot;", '"').replaceAll("&nbsp;", ' ');
-    navigator.clipboard.writeText(code)
-}
-
 function CodeSnippet({ code }: CodeSnippetProps): JSX.Element {
+
+    function copyCode(event: React.MouseEvent<HTMLButtonElement>){
+        var code = String(event.currentTarget.parentElement?.lastElementChild?.lastElementChild?.innerHTML).replaceAll("&amp;", '&',).replaceAll("&gt;", '>').replaceAll("&lt;", '<').replaceAll("&quot;", '"').replaceAll("&nbsp;", ' ');
+        navigator.clipboard.writeText(code)
+    }
+
     return (
         <div className={s.codeBlock}>
             <div className={s.codeBlock_line}></div>
-            <button className={s.codeBlock_copy} onClick={copyCode}><CopyIcon /></button>
+            <button className={s.codeBlock_copy} onClick={copyCode}><CopyIcon className={s.icon_copy} /></button>
             <pre>
                 <code>
                     {code}
